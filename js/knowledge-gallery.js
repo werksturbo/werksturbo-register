80ab3adcac3b4905d1502043e28981efcb231f04
werksturbo <werksturbo@gmx.de>
2026-08-07 13:42:43 +0200

    Create knowledge-gallery.js

---
+++ js/knowledge-gallery.js
@@ -0,0 +1,34 @@
+/******************************************************************
+ * Wissensdatenbank-Galerien
+ ******************************************************************/
+
+const KnowledgeGalleries = {
+
+    kardantunnel: [
+
+        {
+            src: "../images/wissensdb/nr_kardantunnel_gross.jpg",
+            title: "Nummer auf dem Kardantunnel"
+        }
+
+    ],
+
+    karosseriefarbe: [
+
+        {
+            src: "../images/wissensdb/karosserie_farbe.jpg",
+            title: "Karosseriefarbe"
+        }
+
+    ],
+
+    motorcode: [
+
+        {
+            src: "../images/wissensdb/motorcode.jpg",
+            title: "Motorcode PYN"
+        }
+
+    ]
+
+};
