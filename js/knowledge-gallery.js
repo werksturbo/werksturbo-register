2ac5c5a346aee8cc7f52f56e8d7f9ff85ee26aca
werksturbo <werksturbo@gmx.de>
2026-08-10 19:28:46 +0200

    Update knowledge-gallery.js

--- js/knowledge-gallery.js
+++ js/knowledge-gallery.js
@@ -32,3 +32,28 @@ const KnowledgeGalleries = {
     ]
 
 };
+
+/******************************************************************
+ * Wissensdatenbank-Galerie öffnen
+ ******************************************************************/
+
+function openKnowledgeGallery(name, index = 0, title = "") {
+
+    const gallery = KnowledgeGalleries[name];
+
+    if (!gallery) {
+
+        console.warn("Galerie nicht gefunden:", name);
+
+        return;
+
+    }
+
+    console.log("Wissensdatenbank-Galerie:", name);
+    console.log("Bilder:", gallery.length);
+
+    Lightbox.registerGallery(name, gallery);
+
+    Lightbox.openGallery(name, index);
+
+}
