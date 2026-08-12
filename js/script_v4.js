e9627d965f14cb1224f1066a360504060db2be57
werksturbo <werksturbo@gmx.de>
2026-08-12 14:50:32 +0200

    Update script_v4.js

--- js/script_v4.js
+++ js/script_v4.js
@@ -8,7 +8,7 @@
  ******************************************************************/
 
 "use strict";
-console.log("=== CONTROL TEST SCRIPT_V4: ohne Lightbox Navigation");
+console.log("=== CONTROL TEST SCRIPT_V4: ohne Lightbox WissensDB");
 
 /******************************************************************
  * Konfiguration
