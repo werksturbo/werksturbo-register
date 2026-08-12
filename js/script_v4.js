f9931151233523b8d076b0e5dbb7eb2d2f4c1b38
werksturbo <werksturbo@gmx.de>
2026-08-12 19:41:39 +0200

    Update script_v4.js

--- js/script_v4.js
+++ js/script_v4.js
@@ -8,7 +8,7 @@
  ******************************************************************/
 
 "use strict";
-console.log("=== CONTROL TEST SCRIPT_V4: ohne showVehicle.");
+console.log("=== CONTROL TEST SCRIPT_V4: doppelter Formatter gelöscht");
 
 /******************************************************************
  * Konfiguration
