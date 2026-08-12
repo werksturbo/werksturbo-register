27ebcc2decf50192eb2e392d97dcf1590a73f9e0
werksturbo <werksturbo@gmx.de>
2026-08-12 20:06:54 +0200

    Update script_v4.js

--- js/script_v4.js
+++ js/script_v4.js
@@ -8,7 +8,7 @@
  ******************************************************************/
 
 "use strict";
-console.log("=== CONTROL TEST SCRIPT_V4: Thumbnail 90 x 56");
+console.log("=== CONTROL TEST SCRIPT_V4: Thumbnail 100");
 
 /******************************************************************
  * Konfiguration
