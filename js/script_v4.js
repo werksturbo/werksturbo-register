ccaaa33d6e335a3036ce37e51e1d2486578c2d23
werksturbo <werksturbo@gmx.de>
2026-08-12 18:05:21 +0200

    Update script_v4.js

--- js/script_v4.js
+++ js/script_v4.js
@@ -8,7 +8,7 @@
  ******************************************************************/
 
 "use strict";
-console.log("=== CONTROL TEST SCRIPT_V4: ohne Lightbox Informationen");
+console.log("=== CONTROL TEST SCRIPT_V4: ohne showVehicle.");
 
 /******************************************************************
  * Konfiguration
